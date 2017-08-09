69a99634b9d9514a6a4b9b69bfaf1f1de0307497
TuanLogan <anhtuanthpthn@gmail.com>
2017-08-09 07:45:32 +0700

    update login

---
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Support\Facades\Auth;
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/admin';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    public function login(LoginRequest $rq){
+        if (Auth::attempt(['email' => $rq->email, 
+                                'password' => $rq->password])) {
+            // Authentication passed...
+            return redirect()->intended('admin');
+        }
+
+        return redirect(route('login'))->with('msg', 'invalid email/password');
+    }
+}
